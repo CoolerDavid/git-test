c446a7da954dd61e94d99f413ff5cfbc78db66ef
David <David@atguigu.com>
2022-05-26 13:23:59 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Yang
+ * @create 2022-05-26-13:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
